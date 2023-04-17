2f40810c66e28c02caa18026893413b142b71211
Edney Araujo <edney_vk@hotmail.com>
2023-04-17 17:35:59 -0400

    Criando tela login

---
+++ DAL/DadosDaConexao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DadosDaConexao
+    {
+
+        public static String servidor = @"NOTE\SQLEXPRESS";
+        public static String banco = "ControleDeEstoque";
+        public static String usuario = "sa";
+        public static String senha = "39327700";
+
+        public static String StringDeConexao
+        {
+            get
+            {
+                return @"Data Source=" + servidor + ";Initial Catalog=" + banco + ";User ID=" + usuario + ";Password=" + senha;
+                //return "Data Source=SERVERCPD\\SQLEXPRESS;Initial Catalog=ControleDeEstoque;User ID=sa;Password=30232800";
+                //return "Data Source=NOTE-EDNEY\\SQLEXPRESS;Initial Catalog=ControleDeEstoque;User ID=sa;Password=30232800";
+            }
+        }
+    }
+}
--- frm_login.Designer.cs
+++ frm_login.Designer.cs
@@ -28,20 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txt_user = new System.Windows.Forms.TextBox();
+            this.txt_senha = new System.Windows.Forms.TextBox();
+            this.btn_entrar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // panel1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.btn_entrar);
+            this.panel1.Controls.Add(this.txt_senha);
+            this.panel1.Controls.Add(this.txt_user);
+            this.panel1.Location = new System.Drawing.Point(33, 31);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(581, 549);
+            this.panel1.TabIndex = 0;
+            // 
+            // txt_user
+            // 
+            this.txt_user.Location = new System.Drawing.Point(102, 175);
+            this.txt_user.Name = "txt_user";
+            this.txt_user.Size = new System.Drawing.Size(250, 26);
+            this.txt_user.TabIndex = 0;
+            // 
+            // txt_senha
+            // 
+            this.txt_senha.Location = new System.Drawing.Point(102, 224);
+            this.txt_senha.Name = "txt_senha";
+            this.txt_senha.Size = new System.Drawing.Size(250, 26);
+            this.txt_senha.TabIndex = 1;
+            // 
+            // btn_entrar
+            // 
+            this.btn_entrar.Location = new System.Drawing.Point(102, 286);
+            this.btn_entrar.Name = "btn_entrar";
+            this.btn_entrar.Size = new System.Drawing.Size(119, 52);
+            this.btn_entrar.TabIndex = 2;
+            this.btn_entrar.Text = "Entrar";
+            this.btn_entrar.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(243, 286);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 52);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // frm_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(429, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(644, 692);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frm_login";
+            this.Text = "ENTRAR - LOGIN";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txt_senha;
+        private System.Windows.Forms.TextBox txt_user;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn_entrar;
     }
 }
 
