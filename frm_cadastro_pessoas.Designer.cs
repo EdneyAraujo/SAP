14de06323d41de1d3ef2ee05b858d9a13bcb6204
Edney Araujo <edney_vk@hotmail.com>
2023-04-19 18:22:26 -0400

    inicio implementação cadastro pessoas

--- frm_cadastro_pessoas.Designer.cs
+++ frm_cadastro_pessoas.Designer.cs
@@ -28,12 +28,280 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnDados = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRsocial = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textCPFCNPJ = new System.Windows.Forms.TextBox();
+            this.RG = new System.Windows.Forms.Label();
+            this.textRGIE = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCep = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textEstado = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textCidade = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textRua = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textNumero = new System.Windows.Forms.TextBox();
+            this.pnDados.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnDados
+            // 
+            this.pnDados.Controls.Add(this.label9);
+            this.pnDados.Controls.Add(this.textNumero);
+            this.pnDados.Controls.Add(this.label8);
+            this.pnDados.Controls.Add(this.textRua);
+            this.pnDados.Controls.Add(this.label7);
+            this.pnDados.Controls.Add(this.textCidade);
+            this.pnDados.Controls.Add(this.label6);
+            this.pnDados.Controls.Add(this.textEstado);
+            this.pnDados.Controls.Add(this.label5);
+            this.pnDados.Controls.Add(this.txtCep);
+            this.pnDados.Controls.Add(this.RG);
+            this.pnDados.Controls.Add(this.textRGIE);
+            this.pnDados.Controls.Add(this.label4);
+            this.pnDados.Controls.Add(this.textCPFCNPJ);
+            this.pnDados.Controls.Add(this.label3);
+            this.pnDados.Controls.Add(this.txtRsocial);
+            this.pnDados.Controls.Add(this.label2);
+            this.pnDados.Controls.Add(this.textBox1);
+            this.pnDados.Controls.Add(this.label1);
+            this.pnDados.Controls.Add(this.txtNome);
+            this.pnDados.Location = new System.Drawing.Point(12, 12);
+            this.pnDados.Name = "pnDados";
+            this.pnDados.Size = new System.Drawing.Size(849, 411);
+            this.pnDados.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(12, 429);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(849, 74);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(8, 87);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(205, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "NOME";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(8, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(53, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "CODIGO";
+            // 
+            // txtRsocial
+            // 
+            this.txtRsocial.Location = new System.Drawing.Point(8, 139);
+            this.txtRsocial.Name = "txtRsocial";
+            this.txtRsocial.Size = new System.Drawing.Size(253, 20);
+            this.txtRsocial.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "RAZÃO SOCIAL";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "CPF";
+            // 
+            // textCPFCNPJ
+            // 
+            this.textCPFCNPJ.Location = new System.Drawing.Point(8, 193);
+            this.textCPFCNPJ.Name = "textCPFCNPJ";
+            this.textCPFCNPJ.Size = new System.Drawing.Size(143, 20);
+            this.textCPFCNPJ.TabIndex = 6;
+            // 
+            // RG
+            // 
+            this.RG.AutoSize = true;
+            this.RG.Location = new System.Drawing.Point(185, 177);
+            this.RG.Name = "RG";
+            this.RG.Size = new System.Drawing.Size(23, 13);
+            this.RG.TabIndex = 9;
+            this.RG.Text = "RG";
+            // 
+            // textRGIE
+            // 
+            this.textRGIE.Location = new System.Drawing.Point(182, 193);
+            this.textRGIE.Name = "textRGIE";
+            this.textRGIE.Size = new System.Drawing.Size(143, 20);
+            this.textRGIE.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 228);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "CEP";
+            // 
+            // txtCep
+            // 
+            this.txtCep.Location = new System.Drawing.Point(8, 244);
+            this.txtCep.Name = "txtCep";
+            this.txtCep.Size = new System.Drawing.Size(143, 20);
+            this.txtCep.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(185, 228);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "ESTADO";
+            // 
+            // textEstado
+            // 
+            this.textEstado.Location = new System.Drawing.Point(182, 244);
+            this.textEstado.Name = "textEstado";
+            this.textEstado.Size = new System.Drawing.Size(143, 20);
+            this.textEstado.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(366, 228);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "CIDADE";
+            // 
+            // textCidade
+            // 
+            this.textCidade.Location = new System.Drawing.Point(363, 244);
+            this.textCidade.Name = "textCidade";
+            this.textCidade.Size = new System.Drawing.Size(143, 20);
+            this.textCidade.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(11, 277);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(30, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "RUA";
+            // 
+            // textRua
+            // 
+            this.textRua.Location = new System.Drawing.Point(8, 293);
+            this.textRua.Name = "textRua";
+            this.textRua.Size = new System.Drawing.Size(317, 20);
+            this.textRua.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(366, 277);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(55, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "NUMERO";
+            // 
+            // textNumero
+            // 
+            this.textNumero.Location = new System.Drawing.Point(363, 293);
+            this.textNumero.Name = "textNumero";
+            this.textNumero.Size = new System.Drawing.Size(143, 20);
+            this.textNumero.TabIndex = 18;
+            // 
+            // frm_cadastro_pessoas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frm_cadastro_pessoas";
+            this.ClientSize = new System.Drawing.Size(873, 515);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pnDados);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frm_cadastro_pessoas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CADASTRO DE PESSOAS";
+            this.pnDados.ResumeLayout(false);
+            this.pnDados.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnDados;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textCPFCNPJ;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRsocial;
+        private System.Windows.Forms.Label RG;
+        private System.Windows.Forms.TextBox textRGIE;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCep;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textCidade;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textEstado;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textNumero;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textRua;
     }
 }
